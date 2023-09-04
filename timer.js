9a2fc34cc89c1baffb09fa9e4718e348badc66e8
ethanly <ethanly8617@gmail.com>
2023-09-04 13:03:18 -0500

    Ao5, Ao12, Ao50, Ao100 statistic working

--- timer.js
+++ timer.js
@@ -14,9 +14,13 @@ var bestSolve ;
 var appendAvg = document.getElementById("avg") ;
 var avg ;
 var appendAo5 = document.getElementById("ao5") ;
-var ao12 = document.getElementById("ao12") ;
-var ao50 = document.getElementById("ao50") ;
-var ao100 = document.getElementById("ao100") ;
+var ao5 ;
+var appendAo12 = document.getElementById("ao12") ;
+var ao12 ;
+var appendAo50 = document.getElementById("ao50") ;
+var ao50 ;
+var appendAo100 = document.getElementById("ao100") ;
+var ao100 ;
 
 var modal = document.querySelector(".modal") ;
 var overlay = document.querySelector(".overlay") ;
@@ -63,24 +67,77 @@ function updateStats(time)
         appendAvg.innerHTML = time ;
         avg = parseFloat(time) ;
     }//end if
-    else if(solveArr.length > 1)
+
+    /* Best Solve and Avg Statistic */
+    if(solveArr.length > 1)
     {
         if(parseFloat(time) < bestSolve)
         {
             bestSolve = parseFloat(time) ;
             appendBestSolve.innerHTML = time ;
         }//end if
-        //avg = (avg+parseFloat(time))/solveArr.length ;
-        var temp = 0 ;
+
+        var avgtemp = 0 ;
         for(let i=0; i<solveArr.length; i++) 
         {
-            temp+=parseFloat(solveArr[i].textContent) ;
+            avgtemp += parseFloat(solveArr[i].textContent) ;
         }//end for i
 
-        avg = temp/solveArr.length ;
-
+        avg = avgtemp/solveArr.length ;
         appendAvg.innerHTML = avg.toFixed(2) ;
     }//end else if
+
+    /* Ao5 Statistic */
+    if(solveArr.length >= 5)
+    {
+        var ao5temp = 0 ;
+        for(let i=0; i<5; i++)
+        {
+            ao5temp += parseFloat(solveArr[i].textContent) ;
+        }//end for i
+
+        ao5 = ao5temp/5 ;
+        appendAo5.innerHTML = ao5.toFixed(2) ;
+    }//end if
+
+    /* Ao12 Statistic */
+    if(solveArr.length >= 12)
+    {
+        var ao12temp = 0 ;
+        for(let i=0; i<12; i++)
+        {
+            ao12temp += parseFloat(solveArr[i].textContent) ;
+        }//end for i
+
+        ao12 = ao12temp/12 ;
+        appendAo12.innerHTML = ao12.toFixed(2) ;
+    }//end if
+
+    /* Ao50 Statistic */
+    if(solveArr.length >= 50)
+    {
+        var ao50temp = 0 ;
+        for(let i=0; i<50; i++)
+        {
+            ao50temp += parseFloat(solveArr[i].textContent) ;
+        }//end for i
+
+        ao50 = ao50temp/50 ;
+        appendAo50.innerHTML = ao50.toFixed(2) ;
+    }
+
+    /* Ao100 Statistic */
+    if(solveArr.length >= 100)
+    {
+        var ao100temp = 0 ;
+        for(let i=0; i<100; i++)
+        {
+            ao100temp += parseFloat(solveArr[i].textContent) ;
+        }//end for i
+
+        ao100 = ao100temp/100 ;
+        appendAo100.innerHTML = ao100.toFixed(2) ;
+    }
 
 }
 
