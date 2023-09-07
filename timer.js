697c4bf59a11374a3ecf100397c3c2ab957e3d0f
ethanly <ethanly8617@gmail.com>
2023-09-07 00:52:38 -0400

    Valid scrambles - no side is turned 2x in a row

--- timer.js
+++ timer.js
@@ -190,6 +190,7 @@ document.addEventListener('keyup', (event) => {
         solveList.prepend(li) ;
 
         updateStats(time.innerHTML) ;
+        generateScramble() ;
     }//end else if
 
     else if(keyName  == "r")
@@ -207,7 +208,7 @@ document.addEventListener('keyup', (event) => {
         {
             appendBestSolve.innerHTML = "--" ;
             appendAvg.innerHTML = "--" ;
-        }
+        }//end if
     }//end else if
 
     if(keyName == "c")
@@ -231,20 +232,63 @@ function getRandomNum(max)
     return Math.floor(Math.random() * Math.floor(max)) ;
 }
 
+function getRandomNumBetween(min, max)
+{
+    return Math.floor(Math.random()*(max-min)+min) ;
+}
+
 function generateScramble() 
 {
     const moveList = ["F", "F'", "F2", "R", "R'", "R2", 
                     "L", "L'", "L2", "U", "U'", "U2", 
                     "D", "D'", "D2", "B", "B'", "B2"
-                    ] ;
-    var scramble = [] ; 
+                    ] ; 
+    const NUM_MOVE_TYPE = 6 ; //F, B, R, L, U, D
+    var scramble_int = [] ; 
+    var scramble_char = [] ;
     var scrambleStr ; 
-
-    for(let i=0; i<20; i++)
+    var isValidScramble = false ;
+    
+    //Ensure that no side is turned twice in a row
+    scramble_int.push(getRandomNum(NUM_MOVE_TYPE)) ;
+    var idx = 1 ;
+    while(idx < 20) 
     {
-        scramble.push(moveList[getRandomNum(moveList.length)]) ;  
+        var temp = getRandomNum(NUM_MOVE_TYPE) ;
+
+        while(temp == scramble_int[idx-1])
+            temp = getRandomNum(NUM_MOVE_TYPE) ;
+
+        scramble_int.push(temp) ;  
+        idx++ ;
+    }//end for i
+    
+    //Choose one of three possible turns depending on the side
+    for(let i=0; i<scramble_int.length; i++)
+    {
+        switch (scramble_int[i]) 
+        {
+            case 0: 
+                scramble_char.push(moveList[getRandomNumBetween(0, 3)]) ;
+                break ;
+            case 1:
+                scramble_char.push(moveList[getRandomNumBetween(3, 6)]) ;
+                break ;
+            case 2:
+                scramble_char.push(moveList[getRandomNumBetween(6, 9)]) ;
+                break ;
+            case 3:
+                scramble_char.push(moveList[getRandomNumBetween(9, 12)]) ;
+                break ;
+            case 4:
+                scramble_char.push(moveList[getRandomNumBetween(12, 15)]) ;
+                break ;
+            case 5:
+                scramble_char.push(moveList[getRandomNumBetween(15, 18)]) ;
+                break ;
+        }//end switch
     }//end for i
 
-    scrambleStr = scramble.toString().split(',').join(' ') ;
+    scrambleStr = scramble_char.toString().split(',').join(' ') ;
     appendScramble.innerHTML = scrambleStr ;
 }//end generateScramble
